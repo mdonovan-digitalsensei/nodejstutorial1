cf651f24031484be1c457086d4b5bb8e37f8d67a
mdonovan-digitalsensei <63415197+mdonovan-digitalsensei@users.noreply.github.com>
2021-05-24 15:11:37 +0100

    added dynamic search

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -17,9 +18,18 @@ class App extends Component {
   }
 
   render() {
-    return (      
-      <div className="App">      
-        <CardList monsters={this.state.monsters} />
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <input
+          type="search"
+          placeholder="Search Monsters"
+          onChange={(e) => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
